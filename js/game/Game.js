d47d032b5ca59201e9e0b271d9fca547370eae39
corpsefilth <robertb@roberto-bahena.com>
2015-01-25 04:05:36 -0800

    bullet enemy collisions

--- js/game/Game.js
+++ js/game/Game.js
@@ -156,6 +156,7 @@ function update() {
 	
 	// Check Collisions
 	game.physics.arcade.overlap(player, greenEnemies, shipCollide, null, this);
+	game.physics.arcade.overlap(greenEnemies,bullets, hitEnemy, null, this);
 }
 
 function fireBullet() {
@@ -238,6 +239,16 @@ function shipCollide(player, enemy) {
 	explosion.alpha = 0.7;
 	explosion.play('explosion', 30, false, true);
 	enemy.kill();
+}
+
+function hitEnemy(enemy, bullet) {
+	var explosion = explosions.getFirstExists(false);
+	explosion.reset(bullet.body.x + bullet.body.halfWidth, bullet.body.y + bullet.body.halfHeight);
+	explosion.body.velocity.y = enemy.body.velocity.y;
+	explosion.alpha = 0.7;
+	explosion.play('explosion', 30, false, true);
+	enemy.kill();
+	bullet.kill();
 }
 
 function render() {
